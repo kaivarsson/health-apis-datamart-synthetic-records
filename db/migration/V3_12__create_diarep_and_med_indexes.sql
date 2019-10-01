0ec94f93bce96a959f0319133a5b2f2b26734bb8
jhulbert <joshua.hulbert@libertyits.com>
2019-10-01 14:36:03 -0400

    DB Indexing for DR and Medication

---
+++ db/migration/V3_12__create_diarep_and_med_indexes.sql
@@ -0,0 +1,7 @@
+/* Diagnostic Report Index */
+Create index [IX_DiagnosticReport_PatientFullICN] on [App].[DiagnosticReport] ([PatientFullICN])
+go
+
+/* Medication Index */
+Create index [IX_Medication_CDWId] on [App].[Medication] ([CDWId])
+go
