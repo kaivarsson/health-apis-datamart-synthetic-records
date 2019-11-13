f9979f3c66d1f1143ef4ca652dc7cfaddf27cd5f
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-11-13 16:52:28 -0500

    .

--- db/migration/V3_15__create_organization.sql
+++ db/migration/V3_15__create_organization.sql
@@ -1,7 +1,7 @@
 Create table [App].[Organization]
 (
-	[CDWID] varchar(15) not null,
-	[NPI] varchar(50) null,
+  [CDWID] varchar(15) not null,
+  [NPI] varchar(50) null,
   [ProviderID] varchar(50) null,
   [EDIID] varchar(50) null,
   [AgencyID] varchar(50) null,
@@ -11,8 +11,8 @@ Create table [App].[Organization]
   [State] varchar(30) null,
   [PostalCode] varchar(30) null,
   [Organization] varchar(max) null,
-	[ETLBatchId] int null,
-	[ETLChunkNum int null,
-	[ETLCreateDate] datetime2(0) null,
-	constraint PK_Organization primary key clustered (CDWID)
+  [ETLBatchId] int null,
+  [ETLChunkNum int null,
+  [ETLCreateDate] datetime2(0) null,
+  constraint PK_Organization primary key clustered (CDWID)
 )
