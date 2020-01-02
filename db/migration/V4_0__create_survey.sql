46427ff614c5356a99d686dee0258113da50eb3d
jhulbert <joshua.hulbert@libertyits.com>
2020-01-02 16:58:55 -0500

    SurveyRecordedDate

--- db/migration/V4_0__create_survey.sql
+++ db/migration/V4_0__create_survey.sql
@@ -5,28 +5,33 @@ Table:          Survey
 Description:    
 
 Data Definition:
-Column Name      | Description
------------------|-----------------------------------------------------
-PatientFullICN   | The patient's VA internal control number.
------------------|-----------------------------------------------------
-SurveyName       | The type of survey that was performed (i.e. Morse Fall Scale). 
------------------|-----------------------------------------------------
-CDWId            | Primary Key. The CDW identifier for the survey.
------------------|-----------------------------------------------------
-Sta3n            | The station identifier of where the survey was performed.
------------------|-----------------------------------------------------
-Survey           | The json formatted survey datamart resource payload.
------------------|-----------------------------------------------------
-ETLBatchId       | ETL process BatchId
------------------|-----------------------------------------------------
-ETLChunkNum      | ETL process chunk number  Used for managing initial 
-                 | loading and updates of records
------------------|-----------------------------------------------------
-ETLCreateDate    | Date and time in UTC ETL record was first created
------------------|-----------------------------------------------------
-ETLEditDate      | Date and time in UTC ETL batch process last updated
-                 | the record
------------------|-----------------------------------------------------
+Column Name        | Description
+-------------------|-----------------------------------------------------
+PatientFullICN     | The patient's VA internal control number.
+-------------------|-----------------------------------------------------
+SurveyName         | The type of survey that was performed (i.e. Morse 
+                   | Fall Scale). 
+-------------------|-----------------------------------------------------
+CDWId              | Primary Key. The CDW identifier for the survey.
+-------------------|-----------------------------------------------------
+Sta3n              | The station identifier of where the survey was 
+                   | performed.
+-------------------|-----------------------------------------------------
+SurveyRecordedDate | The date the survey was recorded by the person 
+                   | administering the survey
+-------------------|-----------------------------------------------------
+Survey             | The json formatted survey datamart resource payload.
+-------------------|-----------------------------------------------------
+ETLBatchId         | ETL process BatchId
+-------------------|-----------------------------------------------------
+ETLChunkNum        | ETL process chunk number  Used for managing initial 
+                   | loading and updates of records
+-------------------|-----------------------------------------------------
+ETLCreateDate      | Date and time in UTC ETL record was first created
+-------------------|-----------------------------------------------------
+ETLEditDate        | Date and time in UTC ETL batch process last updated
+                   | the record
+-------------------|-----------------------------------------------------
 
 ========================================================================== */
 
@@ -37,6 +42,7 @@ Create table [app].[Survey]
         [DateUTC] datetime null,
         [SurveyName] varchar(50) not null,
         [Sta3n] varchar(50) not null,
+        [SurveyRecordedDate] datetime not null,
         [Survey] varchar(max) null,
         [ETLBatchId] int null,
         [ETLChunkNum] int null,
@@ -49,7 +55,7 @@ go
 Create index [IX_Survey_PatientFullICN] on [App].[Survey]([PatientFullICN])
 go
 
-create index [IX_Survey_Reported] on [App].[Survey]([DateUTC])
+create index [IX_Survey_Recorded_Date] on [App].[Survey]([SurveyRecordedDate])
 go
 
 create index [IX_Survey_Type] on [App].[Survey]([SurveyName])
