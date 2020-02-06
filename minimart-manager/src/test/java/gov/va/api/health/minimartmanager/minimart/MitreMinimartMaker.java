c65a17dd332271c290c90f665325190a96970c95
Bryan Schofield <bryan.schofield@libertyits.com>
2020-02-06 11:29:31 -0500

    MMM can now support Fall Risk ndjson files

--- minimart-manager/src/test/java/gov/va/api/health/minimartmanager/minimart/MitreMinimartMaker.java
+++ minimart-manager/src/test/java/gov/va/api/health/minimartmanager/minimart/MitreMinimartMaker.java
@@ -233,8 +233,19 @@ public class MitreMinimartMaker {
 
   @SneakyThrows
   private void insertByFallRisk(File file) {
-    DatamartFallRisk dm = JacksonConfig.createMapper().readValue(file, DatamartFallRisk.class);
+    insertByFallRiskPayload(fileToString(file));
+  }
+
+  @SneakyThrows
+  private void insertByFallRiskNd(File file) {
+    Files.lines(file.toPath()).forEach(this::insertByFallRiskPayload);
+  }
+
+  @SneakyThrows
+  private void insertByFallRiskPayload(String payload) {
+    DatamartFallRisk dm = JacksonConfig.createMapper().readValue(payload, DatamartFallRisk.class);
     String cdwId = dm.cdwId();
+    // TODO - ENTITY NEEDS TO BE UPDATED WITH NEW COLUMNS
     FallRiskEntity entity =
         FallRiskEntity.builder()
             .cdwId(cdwId)
@@ -243,7 +254,7 @@ public class MitreMinimartMaker {
             .station(dm.station())
             .morseScore(dm.morseScore())
             .morseCategory(dm.morseCategory())
-            .payload(fileToString(file))
+            .payload(payload)
             .build();
     save(entity, cdwId);
   }
@@ -440,6 +451,7 @@ public class MitreMinimartMaker {
         break;
       case "FallRisk":
         insertResourceByPattern(dmDirectory, "^dmFalRis.*json$", this::insertByFallRisk);
+        insertResourceByPattern(dmDirectory, "^dmFalRis.*ndjson$", this::insertByFallRiskNd);
         break;
       case "Immunization":
         insertResourceByPattern(dmDirectory, "^dmImm.*json$", this::insertByImmunization);
