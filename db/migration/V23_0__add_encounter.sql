a8edb407c04548a3acf149c9fd37376cf59b8889
taylorrassmannlibertyits <64798603+taylorrassmannlibertyits@users.noreply.github.com>
2021-12-03 10:16:20 -0500

    API-11684 add Encounter table (#225)

---
+++ db/migration/V23_0__add_encounter.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [App].[Encounter](
+    [CDWIdNumber] [bigint] NOT NULL,
+    [CDWIdResourceCode] [char(1)] NOT NULL,
+    [PatientFullICN] [varchar](50) NOT NULL,
+    [StartDateTime] [datetime2(0)] NULL,
+    [EndDateTime] [datetime2(0)] NULL,
+    [LastUpdated] [date] NOT NULL,
+    [Encounter]  varchar(max)
+    CONSTRAINT PK_Encounter PRIMARY KEY CLUSTERED (CDWIdNumber, CDWIdResourceCode)
+)
+GO
